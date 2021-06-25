ca3739aaad804c2510eae1568aa0c090cdabc49a
hugoeomarques <75944913+hugoeomarques@users.noreply.github.com>
2021-06-25 02:28:16 +0100

    home layout quase pronto

--- Web/frontend/src/views/home.js
+++ Web/frontend/src/views/home.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ChartistGraph from "react-chartist";
 import './style_auxiliar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import DropdownButton from 'react-bootstrap/DropdownButton'
 // react-bootstrap components
 import {
+  Dropdown,
   Badge,
   Button,
   Card,
@@ -41,208 +45,27 @@ function home() {
                     </div>
                     </div>
                 </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  Ir para lotação
-                </div>
-              </Card.Footer>
+              </Card.Body>  
             </Card>
           </Col>   {/*coluna Estatisticas Diárias*/}
-          <Col lg="3" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-light-3 text-success"></i>
-                    </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Revenue</p>
-                      <Card.Title as="h4">$ 1,345</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="far fa-calendar-alt mr-1"></i>
-                  Last day
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col lg="3" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-vector text-danger"></i>
-                    </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Errors</p>
-                      <Card.Title as="h4">23</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="far fa-clock-o mr-1"></i>
-                  In the last hour
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col lg="3" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-favourite-28 text-primary"></i>
-                    </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Followers</p>
-                      <Card.Title as="h4">+45K</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-redo mr-1"></i>
-                  Update now
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md="8">
+         <Col md="7">
             <Card>
               <Card.Header>
-                <Card.Title as="h4">Users Behavior</Card.Title>
-                <p className="card-category">24 Hours performance</p>
-              </Card.Header>
-              <Card.Body>
-                <div className="ct-chart" id="chartHours">
-                  <ChartistGraph
-                    data={{
-                      labels: [
-                        "9:00AM",
-                        "12:00AM",
-                        "3:00PM",
-                        "6:00PM",
-                        "9:00PM",
-                        "12:00PM",
-                        "3:00AM",
-                        "6:00AM",
-                      ],
-                      series: [
-                        [287, 385, 490, 492, 554, 586, 698, 695],
-                        [67, 152, 143, 240, 287, 335, 435, 437],
-                        [23, 113, 67, 108, 190, 239, 307, 308],
-                      ],
-                    }}
-                    type="Line"
-                    options={{
-                      low: 0,
-                      high: 800,
-                      showArea: false,
-                      height: "245px",
-                      axisX: {
-                        showGrid: false,
-                      },
-                      lineSmooth: true,
-                      showLine: true,
-                      showPoint: true,
-                      fullWidth: true,
-                      chartPadding: {
-                        right: 50,
-                      },
-                    }}
-                    responsiveOptions={[
-                      [
-                        "screen and (max-width: 640px)",
-                        {
-                          axisX: {
-                            labelInterpolationFnc: function (value) {
-                              return value[0];
-                            },
-                          },
-                        },
-                      ],
-                    ]}
-                  />
-                </div>
-              </Card.Body>
-              <Card.Footer>
-                <div className="legend">
-                  <i className="fas fa-circle text-info"></i>
-                  Open <i className="fas fa-circle text-danger"></i>
-                  Click <i className="fas fa-circle text-warning"></i>
-                  Click Second Time
-                </div>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-history"></i>
-                  Updated 3 minutes ago
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col md="4">
-            <Card>
-              <Card.Header>
-                <Card.Title as="h4">Email Statistics</Card.Title>
-                <p className="card-category">Last Campaign Performance</p>
-              </Card.Header>
-              <Card.Body>
-                <div
-                  className="ct-chart ct-perfect-fourth"
-                  id="chartPreferences"
-                >
-                  <ChartistGraph
-                    data={{
-                      labels: ["40%", "20%", "40%"],
-                      series: [40, 20, 40],
-                    }}
-                    type="Pie"
-                  />
-                </div>
-                <div className="legend">
-                  <i className="fas fa-circle text-info"></i>
-                  Open <i className="fas fa-circle text-danger"></i>
-                  Bounce <i className="fas fa-circle text-warning"></i>
-                  Unsubscribe
-                </div>
-                <hr></hr>
-                <div className="stats">
-                  <i className="far fa-clock"></i>
-                  Campaign sent 2 days ago
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md="6">
-            <Card>
-              <Card.Header>
-                <Card.Title as="h4">2017 Sales</Card.Title>
-                <p className="card-category">All products including Taxes</p>
+
+              <p className="first_titulo_esquerda">Reportes:
+              <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic" className="dropdown_style">
+    Periodo de Tempo
+  </Dropdown.Toggle>
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Tempo real</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Hoje</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Esta semana</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown></p>
+
+                
+                <p className="card-category">Todos os reportes do local:</p>
               </Card.Header>
               <Card.Body>
                 <div className="ct-chart" id="chartActivity">
@@ -277,20 +100,6 @@ function home() {
                           756,
                           895,
                         ],
-                        [
-                          412,
-                          243,
-                          280,
-                          580,
-                          453,
-                          353,
-                          300,
-                          364,
-                          368,
-                          410,
-                          636,
-                          695,
-                        ],
                       ],
                     }}
                     type="Bar"
@@ -317,20 +126,116 @@ function home() {
                   />
                 </div>
               </Card.Body>
-              <Card.Footer>
-                <div className="legend">
-                  <i className="fas fa-circle text-info"></i>
-                  Tesla Model S <i className="fas fa-circle text-danger"></i>
-                  BMW 5 Series
-                </div>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-check"></i>
-                  Data information certified
-                </div>
-              </Card.Footer>
             </Card>
           </Col>
+        </Row>
+
+        <Row>
+          <Col md="7">
+            <Card>
+              <Card.Header>
+               <p className="first_titulo_esquerda">Lotação:
+              <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic" className="dropdown_style_lotacao">
+   Local Indoor
+  </Dropdown.Toggle>
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Cantina</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">BAR</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">bla bla</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown></p>
+                <p className="card-category">Dados dos últimos 7 dias</p>
+              </Card.Header>
+              <Card.Body>
+                <div className="ct-chart" /*id="chartHours"*/>
+                  <ChartistGraph
+                    data={{
+                      labels: [
+                        "S",
+                        "T",
+                        "Q",
+                        "Q",
+                        "S",
+                        "S",
+                        "D",
+                      ],
+                      series: [
+                        [287, 385, 490, 492, 554, 586, 698],
+                        /*[67, 152, 143, 240, 287, 335, 435],
+                        [23, 113, 67, 108, 190, 239, 307],*/
+                      ],
+                    }}
+                    type="Line"
+                    options={{
+                      low: 0,
+                      high: 800,
+                      showArea: false,
+                      height: "245px",
+                      axisX: {
+                        showGrid: false,
+                      },
+                      lineSmooth: true,
+                      showLine: true,
+                      showPoint: true,
+                      fullWidth: true,
+                      chartPadding: {
+                        right: 50,
+                      },
+                    }}
+                    responsiveOptions={[
+                      [
+                        "screen and (max-width: 640px)",
+                        {
+                          axisX: {
+                            labelInterpolationFnc: function (value) {
+                              return value[0];
+                            },
+                          },
+                        },
+                      ],
+                    ]}
+                  />
+                </div>
+              </Card.Body>
+              
+            </Card>
+          </Col>
+          <Col md="4">
+            <Card>
+              <Card.Header>
+              
+              <p className="first_titulo">Estatística global da Instituição:</p>
+               
+                <hr></hr>
+                
+              </Card.Header>
+              <Card.Body>
+                <div
+                  className="ct-chart ct-perfect-fourth"
+                  id="chartPreferences"
+                >
+                  <ChartistGraph
+                    data={{
+                      labels: ["90%", "10%"],
+                      series: [90, 10],
+                    }}
+                    type="Pie"
+                  />
+                </div>
+                <div className="legend">
+                  
+                  <i className="fas fa-circle text-danger"></i>Utilizadores Instituição  <br/>
+                  <i className="fas fa-circle text-info"></i> Registos Globais
+                  
+                </div>
+                
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          
           <Col md="6">
             <Card className="card-tasks">
               <Card.Header>
