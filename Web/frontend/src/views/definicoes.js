204ff1b175c7f121c4c5e1ddb75e69799c41ec8e
DiogoTojal <estgv17172@alunos.estgv.ipv.pt>
2021-06-29 19:48:52 +0100

    Definições mais alterações
    Mais alterações nas definicões

--- Web/frontend/src/views/definicoes.js
+++ Web/frontend/src/views/definicoes.js
@@ -2,15 +2,19 @@ import React from "react";
 
 // react-bootstrap components
 import {
+  Dropdown,
   Badge,
   Button,
   Card,
-  Form,
   Navbar,
   Nav,
+  Table,
   Container,
   Row,
   Col,
+  Form,
+  OverlayTrigger,
+  Tooltip,
 } from "react-bootstrap";
 
 function User() {
@@ -50,28 +54,107 @@ function User() {
                 <Card.Title as="h4">Outras informações:</Card.Title>
               </Card.Header>
               <Card.Body>
-              
-              <div class="form-group">
-                  <label class="col-md-4 control-label">First Name</label>  
-                  <div class="col-md-4 inputGroupContainer">
-                  <div class="input-group">
-                  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                  <input  name="first_name" placeholder="First Name" class="form-control"  type="text"></input>
-              </div>
-        </div>
-      </div>
-     
+                <Row style={{paddingLeft:'2%'}}>                 
+                  <form class="row row-cols-lg-auto g-3 align-items-center">
+                    <div class="col-12">
+                      <p>
+                        
+                        <div class="input-group">
+                        <b style={{paddingRight:'5%', paddingTop:'1.5%'}}>Nome:</b>
+                          <div class="input-group-text"><i class="fas fa-user-circle"></i></div>
+                          <input  type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Nome instituição" title="Nome da instituição"/>
+                        </div>
+                        </p>
+                    </div>
+                </form>
+                </Row>
+                <Row style={{paddingLeft:'2%'}}>                 
+                  <form class="row row-cols-lg-auto g-3 align-items-center">
+                    <div class="col-12">
+                      <p>
+                        
+                        <div class="input-group">
+                        <b style={{paddingRight:'5%', paddingTop:'1.5%'}}>Email:</b>
+                          <div class="input-group-text"><i class="fas fa-envelope-open-text"></i></div>
+                          <input  type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Email instituição" title="Email da instituição"/>
+                        </div>
+                        </p>
+                    </div>
+                </form>
+                </Row>
 
+                <Row style={{paddingLeft:'2%'}}>                 
+                  <form class="row row-cols-lg-auto g-3 align-items-center">
+                    <div class="col-12">
+                      <p>
+                        
+                        <div class="input-group">
+                        <b style={{paddingRight:'5%', paddingTop:'1.5%'}}>Telefone:</b>
+                       
+                            <div class="input-group-text"><i class="fas fa-phone"></i></div>
+                            <input  type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Telefone da instituição" title="Telefone da instituição"/>
+                          
+                        </div>
+                        </p>
+                    </div>
+                </form>
+                </Row>
+              
+                <Row style={{paddingLeft:'2%'}}>                 
+                  <form class="row row-cols-lg-auto g-3 align-items-center">
+                    <div class="col-12">
+                      <p>
+                        
+                        <div class="input-group">
+                        <b style={{paddingRight:'5%', paddingTop:'1.5%'}}>Telemóvel:</b>
+                          <div class="input-group-text"><i class="fas fa-mobile-alt"></i></div>
+                          <input  type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Telemóvel da instituição" title="Telemóvel da instituição"/>
+                        </div>
+                        </p>
+                    </div>
+                </form>
+                </Row>
+
+                <Row style={{paddingLeft:'2%'}}>                 
+                  <form class="row row-cols-lg-auto g-3 align-items-center">
+                    <div class="col-12">
+                      <p>
+                        
+                        <div class="input-group">
+                        <b style={{paddingRight:'5%', paddingTop:'1.5%'}}>Detalhes:</b>
+                          <div class="input-group-text"><i class="fas fa-info-circle"></i></div>
+                          <textarea class="form-control" id="exampleFormControlTextarea1" rows="6" cols="10" placeholder="Descrição da instituição" title="Descrição da instituição" ></textarea>
+                        </div>
+                        </p>
+                    </div>
+                </form>
+                </Row>
+                <Row style={{paddingLeft:'2%'}}>                 
+                  <form class="row row-cols-lg-auto g-3 align-items-center">
+                    <div class="col-12">
+                      <p>
+                        
+                        <div class="input-group">
+                        <b style={{paddingRight:'5%', paddingTop:'1.5%'}}>Imagem:</b>
+                          <div class="input-group-text"><i class="fas fa-file-image"></i></div>
+                          <button style={{width:'74.5%', backgroundColor:'green'}} type="button" class="btn btn-success"><i class="fas fa-plus-circle" style={{color:'black'}}></i><b style={{color:'black', paddingLeft:'5%'}} >Adicionar</b></button>
+
+                        </div>
+                        </p>
+                    </div>
+                </form>
+                </Row>
 
 
 
                 <Row style={{paddingTop:'2%'}}>
                   
-                <button style={{width:'50%', backgroundColor:'green', marginLeft:'25%'}} type="button" class="btn btn-success"><i class="fa fa-save" style={{color:'black'}}></i><b style={{color:'black', paddingLeft:'5%'}} >Gravar</b></button>
+                <button style={{width:'50%', backgroundColor:'green', marginLeft:'25%'}} type="button" class="btn btn-success"><i class="fa fa-save" style={{color:'black'}}></i><b style={{color:'black', paddingLeft:'5%'}} >Gravar Alterações</b></button>
                 </Row>
               </Card.Body>
             </Card>
           </Col>
+
 
           <Col md="6">
           <Card>
@@ -106,6 +189,99 @@ function User() {
             </Card>
           </Col>
         </Row>
+
+
+        <Row>
+          <Col md="12">
+            <Card className="strpied-tabled-with-hover">
+              <Card.Header>
+                <Card.Title as="h4">Locais Outdoor</Card.Title>
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic" className="dropdown_style_utilizadorespendentes">
+                    Ordenar por nome: 
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Ascendente</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Descendente</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                
+              </Card.Header>
+              <Card.Body className="table-full-width table-responsive px-0">
+                <Table className="table-hover">
+                  <thead>
+                    <tr>
+                      <th className="border-0">ID</th>
+                      <th className="border-0">Name</th>
+                      <th className="border-0">Salary</th>
+                      <th className="border-0">Country</th>
+                      <th className="border-0">City</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Dakota Rice</td>
+                      <td>$36,738</td>
+                      <td>Niger</td>
+                      <td>Oud-Turnhout</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Minerva Hooper</td>
+                      <td>$23,789</td>
+                      <td>Curaçao</td>
+                      <td>Sinaai-Waas</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Sage Rodriguez</td>
+                      <td>$56,142</td>
+                      <td>Netherlands</td>
+                      <td>Baileux</td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>Philip Chaney</td>
+                      <td>$38,735</td>
+                      <td>Korea, South</td>
+                      <td>Overland Park</td>
+                    </tr>
+                    <tr>
+                      <td>5</td>
+                      <td>Doris Greene</td>
+                      <td>$63,542</td>
+                      <td>Malawi</td>
+                      <td>Feldkirchen in Kärnten</td>
+                    </tr>
+                    <tr>
+                      <td>6</td>
+                      <td>Mason Porter</td>
+                      <td>$78,615</td>
+                      <td>Chile</td>
+                      <td>Gloucester</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+          </Row>
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
