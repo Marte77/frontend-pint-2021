39ec44038c4d9b81bf358d0f22f0144fbb8919f0
hugoeomarques <75944913+hugoeomarques@users.noreply.github.com>
2021-07-10 02:45:35 +0100

    ficheiros inuteis apagados
    porque vai levar tudo pop up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Admin from "./layouts/Admin";
 import Login from "./view/login";
 import SignUp from "./view/registar";
 import AdminLayout from "layouts/Admin.js";
-import addAlerta from "./views/adicionarAlerta";
 function App() {
   return (<Router>
 
@@ -15,7 +14,6 @@ function App() {
             <Route path="/login" component={Login} />
             <Route path="/registar" component={SignUp} />
             <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-            <Route path="/adicionarAlerta" component={addAlerta} />  
             //após o login, entra na aba
           </Switch>
 
